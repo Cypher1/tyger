45522294c03be3ccec926121940c7bfac7ad8975
J Pratt <jp10010101010000@gmail.com>
2021-05-07 09:50:06 +1000

    Move to typescript

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,13 @@
+import 'mocha';
 import {assert} from 'chai';
 
 describe('basic tests', () => {
     it('runs the tests', () => {
         assert.equal(1, 1, 'simple equality');
+    });
+    it('has types', () => {
+
+        const foo: number = 1;
+        assert.equal(1, foo, 'simple equality');
     });
 });
