d1a95c674f363b90a12fcdbb5ed5b4daa911808a
J Pratt <jp10010101010000@gmail.com>
2021-05-07 11:05:15 +1000

    Start implementing assignablility

--- src/type.ts
+++ src/type.ts
@@ -1,6 +1,15 @@
 export abstract class Type {
 
   abstract toString(): string;
+  abstract canAssignFrom(other: Type): boolean;
+
+  equals(other: Type): boolean {
+    return this.canAssignFrom(other) && other.canAssignFrom(this);
+  }
+
+  isNever(): boolean {
+    return false;
+  }
 }
 
 export class Union extends Type {
@@ -9,13 +18,65 @@ export class Union extends Type {
   }
 
   toString(): string {
-    if (this.types.length === 0) {
+    if (this.isNever()) {
       return `Never`;
     }
     return `(${this.types.map(x => x.toString()).join('|')})`;
   }
+
+  canAssignFrom(other: Type): boolean {
+    for (var type of this.types) {
+      if (type.canAssignFrom(other)) {
+        return true; // `other` can be stored in `type`
+      }
+    }
+    return false;
+  }
+
+  isNever(): boolean {
+    return this.types.length === 0;
+  }
 }
 
+export class Intersection extends Type {
+  constructor(public types: Type[]) {
+    super();
+  }
+
+  toString(): string {
+    if (this.types.length === 0) {
+      return `Unit`;
+    }
+    return `(${this.types.map(x => x.toString()).join('&')})`;
+  }
+
+  canAssignFrom(other: Type): boolean {
+    for (var type of this.types) {
+      if (!type.canAssignFrom(other)) {
+        return false; // other must match all requirements
+      }
+    }
+    return true;
+  }
+}
+
+export class Open extends Type {
+  constructor(public type: Type) {
+    super();
+  }
+
+  toString(): string {
+    if (this.type.isNever()) {
+      return `Any`;
+    }
+    return `${this.type}+`;
+  }
+
+  canAssignFrom(_other: Type): boolean {
+    return true; // open is ...nuts? and accepts anything...
+  }
+}
+/*
 export class Product extends Type {
   constructor(public types: Type[]) {
     super();
@@ -27,4 +88,4 @@ export class Product extends Type {
     }
     return `(${this.types.map(x => x.toString()).join('*')})`;
   }
-}
+}*/
--- test/type.ts
+++ test/type.ts
@@ -1,13 +1,16 @@
 import 'mocha';
 import {assert} from 'chai';
 
-import {Union, Product} from '../src/type.js';
+import {Union, Intersection, Open} from '../src/type.js';
 
 describe('type tests', () => {
   it('constructs Never', () => {
     assert.equal(new Union([]).toString(), 'Never');
   });
   it('constructs Unit', () => {
-    assert.equal(new Product([]).toString(), 'Unit');
+    assert.equal(new Intersection([]).toString(), 'Unit');
+  });
+  it('constructs Open of Never', () => {
+    assert.equal(new Open(new Union([])).toString(), 'Any');
   });
 });
