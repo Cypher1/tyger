ab05fa9f4902d47109c152a0a185bc60fb3e55bc
J Pratt <jp10010101010000@gmail.com>
2021-05-14 09:33:48 +1000

    Reduce duplication in tests

--- test/type.ts
+++ test/type.ts
@@ -61,49 +61,61 @@ describe('type tests', () => {
     assert.isFalse(null_type().equals(undefined_type()), 'null != undefined');
   });
   describe('union type tests', () => {
-    it('a|b', () => {
+    const tys = () => {
       const namedA = named('a', any()); // new type any as a
       const namedB = named('b', any()); // new type any as b
       const aOrB = union(namedA, namedB);
-
+      return { namedA, namedB, aOrB };
+    };
+    it('a is a subtype of a|b', () => {
+      const {namedA, namedB, aOrB} = tys();
       assert.isTrue(namedA.isSubType(aOrB), 'a is a subtype of a|b');
-      assert.isTrue(namedB.isSubType(aOrB), 'a is a subtype of a|b');
+      assert.isTrue(namedB.isSubType(aOrB), 'b is a subtype of a|b');
+    });
+    it('a|b is not a subtype of a', () => {
+      const {namedA, namedB, aOrB} = tys();
       assert.isFalse(aOrB.isSubType(namedA), 'a|b is not a subtype of a');
       assert.isFalse(aOrB.isSubType(namedB), 'a|b is not a subtype of b');
-
-      assert.isFalse(namedA.isSuperType(aOrB), 'a is not a supertype of a|b');
-      assert.isFalse(namedB.isSuperType(aOrB), 'a is not a supertype of a|b');
+    });
+    it('a|b is a supertype of a', () => {
+      const {namedA, namedB, aOrB} = tys();
       assert.isTrue(aOrB.isSuperType(namedA), 'a|b is a supertype of a');
       assert.isTrue(aOrB.isSuperType(namedB), 'a|b is a supertype of b');
     });
+    it('a is not a supertype of a|b', () => {
+      const {namedA, namedB, aOrB} = tys();
+      assert.isFalse(namedA.isSuperType(aOrB), 'a is not a supertype of a|b');
+      assert.isFalse(namedB.isSuperType(aOrB), 'b is not a supertype of a|b');
+    });
   });
   describe('function type tests', () => {
+    const tys = () => {
+      const namedA = named('a', any()); // new type any as a
+      const namedB = named('b', any()); // new type any as b
+      const idA = func(namedA, namedA);
+      const idB = func(namedB, namedB);
+      const aOrB = union(namedA, namedB);
+      return { namedA, namedB, idA, idB, aOrB };
+    };
     it('function type', () => {
-      const namedA = named('a', any());
-      const id = func(namedA, namedA);
+      const id = tys().idA;
       assert.equal(id.toString(), 'a->a');
       assert.isTrue(id.canAssignFrom(id), 'id function is assignable to an id function');
       assert.isTrue(id.equals(id), 'id function equals id function');
     });
     it('non equal function types', () => {
-      const namedA = named('a', any());
-      const namedB = named('b', any());
-      const idA = func(namedA, namedA);
-      const idB = func(namedB, namedB);
+      const {idA, idB} = tys();
       assert.isFalse(idA.canAssignFrom(idB), 'non equal id functions !equals id function');
       assert.isFalse(idB.canAssignFrom(idA), 'non equal id functions !equals id function');
       assert.isFalse(idA.equals(idB), 'idA != idB');
       assert.isFalse(idB.equals(idA), 'idB != idA');
     });
     it('sub typing function types', () => {
-      const namedA = named('a', any()); // new type any as a
-      const namedB = named('b', any()); // new type any as b
-      const aOrB = union(namedA, namedB);
-
+      const {namedA, aOrB} = tys();
       const abstractingId = func(namedA, aOrB);
-      assert.equal(abstractingId.toString(), 'a->(a|b)');
-
       const subtypingId = func(aOrB, namedA);
+
+      assert.equal(abstractingId.toString(), 'a->(a|b)');
       assert.equal(subtypingId.toString(), '(a|b)->a');
 
       assert.isTrue(abstractingId.canAssignFrom(subtypingId), 'non equal id functions !equals id function');
